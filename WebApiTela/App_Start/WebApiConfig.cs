d513c3e52c778cb47413c271f7be7fb1e900057f
programador <garcia.def03@gmail.com>
2020-07-21 14:16:52 -0600

    INIT

--- WebApiTela/App_Start/WebApiConfig.cs
+++ WebApiTela/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace WebApiTela
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
